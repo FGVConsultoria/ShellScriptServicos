3811b4a9e9565ca776eeb34522e9a21f48853aed
Marcelo Giovanni Dias <113317859+FGVConsultoria@users.noreply.github.com>
2022-10-01 13:52:08 -0300

    Create wanguard_dshield.sh
    DShield.org - É uma lista de bloqueios recomendado, resume os top 20 origens de ataque a classe C (/24) e suas sub-redes nos últimos três dias.

---
+++ wanguard_dshield.sh
@@ -0,0 +1,39 @@
+#! /bin/sh
+
+##################################
+# Diretorios
+##################################
+V_Dir="./WanGuard"
+
+##################################
+# Se diretorio não existir cria
+##################################
+[ ! -d "${V_Dir}" ] && mkdir "${V_Dir}"
+
+##################################
+# Variavies do script
+##################################
+V_txt4="${V_Dir}/opennicV4.txt"
+V_msn="${V_Data} - DSHIELD"
+V_mailto="to@mail.net"
+V_Data=`date "+%Y%m%d"`
+V_DT=`date "+%Y"`
+
+##################################
+# Funções
+##################################
+F_grep_ipv4(){
+	grep -oE '((1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])\.){3}(1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])' | sort -h | uniq | sed '/^$/d;s/$/\/24/g'
+}
+
+F_Links(){
+	links -ssl.certificates 0 -dump "https://www.dshield.org/block.txt"
+}
+
+##################################
+# Executa rotinas
+##################################
+
+F_Links | F_grep_ipv4 | awk '(NR == 1) {printf "dst ip "$1} ; (NR > 1) {printf " or dst ip "$1}' > "${V_txt4}"
+
+echo "Dados IPV4 para inclusão nos filtros do dshield ..." | mutt -a "${V_txt4}" -s "${V_msn}" -- "${V_mailto}"
