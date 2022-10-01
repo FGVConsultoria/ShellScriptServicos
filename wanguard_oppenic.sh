6f1a606b5cbcfd32a5a66a8e161fc897a4a16b1e
Marcelo Giovanni Dias <113317859+FGVConsultoria@users.noreply.github.com>
2022-10-01 08:48:25 -0300

    Update wanguard_oppenic.sh
    Correção nomenclatura de uma das funções

--- wanguard_oppenic.sh
+++ wanguard_oppenic.sh
@@ -31,7 +31,7 @@ F_grep_ipv6(){
 	grep -oE '(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))' | sort -h | uniq
 }
 
-F_Opennic(){
+F_Links(){
 	links -ssl.certificates 0 -dump "https://servers.opennic.org/?tier=1"
 	links -ssl.certificates 0 -dump "https://servers.opennic.org/?tier=2"
 }
@@ -40,11 +40,11 @@ F_Opennic(){
 # Executa rotinas
 ##################################
 
-F_Opennic | F_grep_ipv4 | awk '(NR == 1) {printf "dst ip "$1} ; (NR > 1) {printf " or dst ip "$1}' > "${V_txt4}"
+F_Links | F_grep_ipv4 | awk '(NR == 1) {printf "dst ip "$1} ; (NR > 1) {printf " or dst ip "$1}' > "${V_txt4}"
 
 echo "Dados IPV4 para inclusão nos filtros do WanGuard ..." | mutt -a "${V_txt4}" -s "${V_msn}" -- "${V_mailto}"
 
 
-F_Opennic | F_grep_ipv6 | uniq | awk '(NR == 1) {printf "dst ip "$1} ; (NR > 1) {printf " or dst ip "$1}' > "${V_txt6}"
+F_Links | F_grep_ipv6 | uniq | awk '(NR == 1) {printf "dst ip "$1} ; (NR > 1) {printf " or dst ip "$1}' > "${V_txt6}"
 
 echo "Dados IPV6 para inclusão nos filtros do WanGuard ..." | mutt -a "${V_txt6}" -s "${V_msn}" -- "${V_mailto}"
