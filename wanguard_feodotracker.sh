f356dd1ac6047483f8a326073384e0f91d2dc1cb
Marcelo Giovanni Dias <113317859+FGVConsultoria@users.noreply.github.com>
2022-10-01 08:52:02 -0300

    Create wanguard_feodotracker.sh
    Script que cria o arquivo padronizado para o Wanguard a partir dos dados do site https://feodotracker.abuse.ch

---
+++ wanguard_feodotracker.sh
@@ -0,0 +1,40 @@
+#! /bin/sh
+
+##################################
+# Diretorios
+##################################
+V_Dir="./WanGuard"
+
+##################################
+# Se diretorio não existir cria
+##################################
+[ ! -d "${V_Dir}" ] && mkdir "${V_Dir}"
+
+##################################
+# Variavies do script
+##################################
+V_txt4="${V_Dir}/opennicV4.txt"
+V_txt6="${V_Dir}/opennicV6.txt"
+V_msn="${V_Data} - FEODOTRACKER"
+V_mailto="to@mail.net"
+V_Data=`date "+%Y%m%d"`
+V_DT=`date "+%Y"`
+
+##################################
+# Funções
+##################################
+F_grep_ipv4(){
+	grep -oE '((1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])\.){3}(1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])' | sort -h | uniq
+}
+
+F_Links(){
+	links -ssl.certificates 0 -dump "https://feodotracker.abuse.ch/downloads/ipblocklist.txt"
+}
+
+##################################
+# Executa rotinas
+##################################
+
+F_Links | F_grep_ipv4 | awk '(NR == 1) {printf "dst ip "$1} ; (NR > 1) {printf " or dst ip "$1}' > "${V_txt4}"
+
+echo "Dados IPV4 para inclusão nos filtros do Feodotracker ..." | mutt -a "${V_txt4}" -s "${V_msn}" -- "${V_mailto}"
